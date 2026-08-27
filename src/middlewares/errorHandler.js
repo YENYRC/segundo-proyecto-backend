e66792fa9685ac9835255276dece1aa9301f7ecc
YENYRC <yenyruiz389@gmail.com>
2026-08-27 10:43:22 +0200

    Add error logging

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 export const errorHandler = (err, req, res, next) => {
+  console.error("ERROR CAPTURADO:", err);
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
     success: false,
